89c020b9e9bd401077b9e492caf6c48478e01546
Betelihem <Betelihem>
2022-02-16 13:57:34 -0800

    done all 4

--- index.js
+++ index.js
@@ -1,1 +1,72 @@
-// Write your code here...
+// Global Variables
+const url = 'http://localhost:3000/menu'
+let selectedObj
+
+// DOM Selectors
+const menuItemsDiv = document.querySelector('#menu-items')
+// const dishSection = document.querySelector('#dish')
+const dishImage = document.querySelector('#dish-image')
+const dishName = document.querySelector('#dish-name')
+const dishDescription = document.querySelector('#dish-description')
+const dishPrice = document.querySelector('#dish-price')
+
+const cartForm = document.querySelector('#cart-form')
+const cartAmount = document.querySelector('#cart-amount')
+const inCart = document.querySelector('#number-in-cart')
+
+// Listeners
+cartForm.addEventListener('submit', handleToAddCart)
+
+// Fetchers
+function fetchAllMenuItem() {
+    return fetch(url)
+    .then (res=> res.json())
+    // .then (console.log)
+}
+
+// Render Functions
+function renderAllMenuItem(menuArr) {
+    menuArr.forEach(renderOneMenuItem)
+    // console.log(MenuArr)
+    renderDetail(menuArr[0])
+
+}
+
+function renderOneMenuItem(menuObj) {
+    // console.log(MenuObj)
+    selectedObj = menuObj
+
+    let span = document.createElement('span')
+    span.textContent = menuObj.name
+
+    menuItemsDiv.appendChild(span)
+
+    span.addEventListener('click', () => renderDetail(menuObj))
+
+}
+
+function renderDetail(menuObj) {
+    // console.log(menuObj)
+    dishImage.src = menuObj.image
+    dishName.innerText = menuObj.name
+    dishDescription.innerText = menuObj.description
+    dishPrice.innerText = `$${menuObj.price}`
+    inCart.innerText = menuObj.number_in_bag    
+}
+
+// Event Handlers
+function handleToAddCart(e) {
+    e.preventDefault()
+
+    inCart.innerText = (parseInt(cartAmount.value) + parseInt(inCart.innerText)) || 0
+    selectedObj.number_in_bag = inCart.innerText // not persisting
+
+    // console.log(cartAmount.value)
+    // console.log(inCart.innerText)
+    // console.log(selectedObj.number_in_bag)
+
+    e.target.reset()
+}
+
+// Initializer
+fetchAllMenuItem().then(renderAllMenuItem)
